f9249ca91195bf8647b3c17c791ae4441ed5682e
PlasmolysisMango <53737589+PlasmolysisMango@users.noreply.github.com>
2019-09-08 16:24:43 +0800

    Update ss-fly.sh

--- ss-fly.sh
+++ ss-fly.sh
@@ -153,9 +153,8 @@ install_ssr() {
                      yum install -y wget
                      ;;
         esac
-	wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh
-	chmod +x shadowsocksR.sh
-	./shadowsocksR.sh 2>&1 | tee shadowsocksR.log
+	chmod +x ssr.sh
+	./ssr.sh 2>&1 | tee shadowsocksR.log
 }
 
 check_os_() {
